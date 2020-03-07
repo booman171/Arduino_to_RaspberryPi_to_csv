0f7d151ee68bd814ebdc59a20bcb6273a71e85e9
jjohnson3029 <jenario171@yahoo.com>
2020-03-07 16:09:58 -0500

    update

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -10,6 +10,7 @@ ser.flushInput()
 
 # Sets a unique filename or each session
 filename = "data_" + str(time.time()) + ".csv"
+
 while True:
     # Read in Serial line
     ser_bytes = ser.readline()
@@ -33,13 +34,4 @@ while True:
     # Keep file open to write to
     with open(filename, "a") as f:
         # Writes to file separated by commas
-        writer = csv.writer(f)
-        
-        # Add more or less value[x] instances depending on how many data
-        # columns you have.
-        # For example accX, accY, accZ, gyrX, gyrY, gyrZ, would correspond
-        # to values[0], values[1], values[2], value[3], value[4], value[5]
-        # Pay attention to your indexing.
-        writer.writerow(message)
-        
-        #print(str(values))
+        f.write(message)
