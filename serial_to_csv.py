6a0ceab0c27ef18b1f93c82bd39acfdf0befffb7
jjohnson3029 <jenario171@yahoo.com>
2020-03-07 16:24:35 -0500

    update

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -34,4 +34,4 @@ while True:
     # Keep file open to write to
     with open(filename, "a") as f:
         # Writes to file separated by commas
-        f.write(str(time.time()) + message + "\n")
+        f.write(str(time.time()) + "," +  message + "\n")
