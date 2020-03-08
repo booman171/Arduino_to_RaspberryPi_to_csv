b17f24d3a49cd2ce9a43da46683f9244b488ada5
jjohnson3029 <jenario171@yahoo.com>
2020-03-07 20:34:50 -0500

    update

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -6,13 +6,13 @@ import serial
 
 # use the command 'ls /dev/tty*' to deter,ine the com port
 # for your device. in my case it is ttyACM0, as below
-ser = serial.Serial('/dev/ttyUSB0')
+ser = serial.Serial('/dev/ttyACM0')
 ser.flushInput()
 
 # Sets a unique filename or each session
 filename = "data_" + str(time.time()) + ".csv"
 f = open(filename, "a")
-f.write("Epoch,Pitch,Roll,Yaw,Pulse")
+f.write("Epoch,Pitch,Roll,Yaw,Pulse,Temp" + "\n")
 f.close
 while True:
     # Read in Serial line
